0d9adebf85f84193383db33450132e0df6d198d9
novelkhan <novel4004@gmail.com>
2024-01-25 05:23:58 +0600

    Database is added

---
+++ ImgApiForNg/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ImgApiForNg.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //public DbSet<Image> Images { get; set; }
+    }
+}
